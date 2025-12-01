bac61185c6964b0e312ca04979b1e0edca227e6e
ezX <ecubebarbarov@outlook.com>
2025-12-02 00:05:01 +0300

    Update website
    — Scripts added
    — Modified html page

--- lesson_3/website/src/main.ts
+++ lesson_3/website/src/main.ts
@@ -1,1 +1,42 @@
 import './style.scss';
+import $ from 'jquery';
+
+function formatTime(value: number): string {
+    return value < 10 ? "0" + value : value.toString();
+}
+
+function UpdateClock(): void {
+    let now: Date = new Date();
+    let hours: string = formatTime(now.getHours());
+    let minutes: string = formatTime(now.getMinutes());
+    let seconds: string = formatTime(now.getSeconds());
+    
+    let timeString: string = hours + ':' + minutes + ':' + seconds;
+    $(".clock").text(timeString);
+}
+
+$(".burger").on("click", function() {
+    $("header").toggleClass("active"); 
+});
+
+setInterval(function() {
+    UpdateClock();
+}, 1000);
+
+$(window).on("scroll", function() {
+    let windowScrollTop = $(window).scrollTop();
+    if (typeof windowScrollTop === 'undefined') return;
+    
+    $('div[id]').each(function() {
+        let offset = $(this).offset();
+        if (!offset) return;
+        
+        if (windowScrollTop >= offset.top - 100) {
+            const id = $(this).attr('id');
+            if (!id) return;
+            
+            $('.anchor.top a').removeClass('active');
+            $(`.anchor.top a[href="#${id}"]`).addClass('active');
+        }
+    });
+});
