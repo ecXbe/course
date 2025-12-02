0c6c0525ff0c4519384df8f3edde0bb9db82aef2
ezX <ecubebarbarov@outlook.com>
2025-12-02 16:11:09 +0300

    Update website
    — Added validation to "CONTACT"
    — Added bright theme and switching between themes

--- lesson_3/website/src/main.ts
+++ lesson_3/website/src/main.ts
@@ -15,10 +15,57 @@ function UpdateClock(): void {
     $(".clock").text(timeString);
 }
 
-$(".burger").on("click", function() {
-    $("header").toggleClass("active"); 
+function validateName(): boolean {
+    let name = $name.val() as string;
+    if (name.length < 3) {
+        $nameError.text('Минимум 3 символа');
+        return false;
+    }
+    $nameError.text('');
+    return true;
+}
+
+function validateEmail(): boolean {
+    let email = $email.val() as string;
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+        $emailError.text('Некорректный email');
+        return false;
+    }
+    $emailError.text('');
+    return true;
+}
+
+function validateMessage(): boolean {
+    let message = $message.val() as string;
+
+    if (message === "") {
+        $messageError.text("Сообщение должно содержать текст");
+        return false; 
+    }
+    $messageError.text('');
+    return true;
+}
+
+let savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+    $("html").attr('data-theme', savedTheme);
+}
+
+$(".choose_theme").on("click", function() {
+    let currentTheme = $("html").attr('data-theme');
+    let newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+    $("html").attr('data-theme', newTheme);
+    localStorage.setItem("theme", newTheme);
 });
 
+$(".burger").on("click", function() {
+    $(".anchor.top").toggleClass("active"); 
+});
+
+UpdateClock();
 setInterval(function() {
     UpdateClock();
 }, 1000);
@@ -40,3 +87,23 @@ $(window).on("scroll", function() {
         }
     });
 });
+
+let $name = $("input#name");
+let $nameError = $("#nameError");
+
+let $email = $("input#email");
+let $emailError = $("#emailError");
+
+let $message = $("textarea#message");
+let $messageError = $("#messageError");
+
+$name.on('input', () => validateName());
+$email.on('input', () => validateEmail());
+
+let $sendMessageButton = $("button.contact--send");
+$sendMessageButton.on("click", function() {
+    if (validateName() && validateEmail() && validateMessage()) {
+        $(".sent").text("Отправлено");
+    }
+});
+
